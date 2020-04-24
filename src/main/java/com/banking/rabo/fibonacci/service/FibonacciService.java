e894d6d8435d0146eb50bf3ad23b8b4df65b9526
nikitha <nikitaamu@gmail.com>
2020-04-24 22:29:15 +0530

    added streams to iterate

--- src/main/java/com/banking/rabo/fibonacci/service/FibonacciService.java
+++ src/main/java/com/banking/rabo/fibonacci/service/FibonacciService.java
@@ -1,41 +1,39 @@
 package com.banking.rabo.fibonacci.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 /**
- * Interface to calculate fibonacci series
+ * This class is the implementation of the default method in interface
  */
-public interface FibonacciService {
+@Service
+public class FibonacciServiceImpl {
+
 
     /**
-     * Java 8 feature of default method where the implementation of the method
-     * can be present inside the interface with keyword default
+     * This method is used to find the fibonacci series for the given list
      *
      * @param inputValue
-     * @return sum
+     * @return integer list
      */
-    static final Logger log = LoggerFactory.getLogger(FibonacciService.class);
+    public static List<Integer> fibonacci(int inputValue) {
+        return Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+                .limit(inputValue)
+                .map(n -> n[1])
+                .collect(Collectors.toList());
+    }
 
-    default int calculateFibonacci(int inputValue) {
-        int sum;
-        int fibonnaciValue = 0;
-        if (inputValue == 0) {
-            return 0;
-        } else if (inputValue == 1) {
-            return 1;
-        } else {
-            int[] inputArray = new int[inputValue + 1];
-            inputArray[0] = 0;
-            inputArray[1] = 1;
-            sum = inputArray[0] + inputArray[1];
-            for (int i = 2; i <= inputValue; i++) {
-                inputArray[i] = inputArray[i - 1] + inputArray[i - 2];
-                fibonnaciValue = inputArray[i];
-                sum += inputArray[i];
-            }
-            log.info("The fibonacci value for the given input value is {}", fibonnaciValue);
-        }
-        return sum;
+    /**
+     * This method is used to find the sum of the fibonacci series
+     *
+     * @param inputValues
+     * @return
+     */
+    public static int findSumOfFibonacci(List<Integer> inputValues) {
+        return inputValues.stream().mapToInt(Integer::intValue).sum();
     }
 }
